c0eb2db1618645e080560e4a622ca6d4a367d878
Sniperdude <venosabrandon10@gmail.com>
2023-06-08 23:32:45 -0700

    I stg its almost done

--- js/main.js
+++ js/main.js
@@ -3,19 +3,34 @@
 window.onload = () => {
     var banner = document.getElementById("banner");
     var a = 0;
-    var imgs = ["img0", "img1", "img2", "img3", "img4", "img5", "img6", "img7", "img8", "img9"];
+    var baseImgs = ["../img/HighresScreenshot00009.png","../img/image.png","../img/image3.png"];
+    var imgs = shuffle(baseImgs);
+    banner.style.backgroundImage="url("+imgs[a]+")";
+    a++;
     tipTextLoad();
     setInterval( () => {
         if (a >= imgs.length) {
-            a = 0;
-            // Set background image
-            a++;
+            banner.style.backgroundColor="rgba(0,0,0,1)"
+            setTimeout(() => {
+                // More randomizing of images :>
+                imgs = shuffle(baseImgs);
+                a = 0;
+                banner.style.backgroundImage="url("+imgs[a]+")";
+                banner.style.backgroundColor="rgba(0,0,0,0.2)";
+                a++;
+            }, 1100);
+            
         } else {
-            // Set background image
-            a++;
+            banner.style.backgroundColor="rgba(0,0,0,1)"
+            setTimeout(() => {
+                banner.style.backgroundImage="url("+imgs[a]+")";
+                banner.style.backgroundColor="rgba(0,0,0,0.2)";
+                a++;
+            }, 1100);
         }
-    }, 1000);
+    }, 5000);
 
+    // Get that good old navbar to the html page instead of copying and pasting the same navbar to 20 other html pages :>
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
@@ -35,4 +50,15 @@ window.onscroll = () => {
         navbar.style.backgroundColor="rgba(0,0,0,0)"
         navbar.style.backdropFilter="blur(0px)"
     }
+}
+
+function shuffle (array) {
+    let currentIndex = array.length, randomIndex;
+    while (currentIndex != 0) {
+      randomIndex = Math.floor (Math.random () * currentIndex);
+      currentIndex--;
+      [array [currentIndex], array [randomIndex]] = [
+        array [randomIndex], array [currentIndex]];
+    }
+    return array;
 }
