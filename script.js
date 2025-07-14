f890d636a900c1c962c494d8a4bbc30bc2f90fef
KevinKor01 <kevinkor6346789@gmail.com>
2025-07-14 05:01:01 +0300

    Final Update

--- script.js
+++ script.js
@@ -1,34 +1,69 @@
 document.addEventListener('mousemove', (event) => {
     const mouseX = event.clientX;
     const mouseY = event.clientY;
-    
+
     const moveX = (mouseX / window.innerWidth) * 30;
     const moveY = (mouseY / window.innerHeight) * 30;
-    
+
     document.querySelector('.background').style.transform = `translate(-${moveX}px, -${moveY}px)`;
 });
 
-  (function() {
+(function () {
     const konamiSequence = [
-      'ArrowUp', 'ArrowUp',
-      'ArrowDown', 'ArrowDown',
-      'ArrowLeft', 'ArrowRight',
-      'ArrowLeft', 'ArrowRight',
-      'b', 'a'
+        'ArrowUp', 'ArrowUp',
+        'ArrowDown', 'ArrowDown',
+        'ArrowLeft', 'ArrowRight',
+        'ArrowLeft', 'ArrowRight',
+        'b', 'a'
     ];
     let konamiPosition = 0;
+    const secretSequence = ['s', 'e', 'c', 'r', 'e', 't'];
+    let secretPosition = 0;
 
-    window.addEventListener('keydown', function(e) {
-      const key = e.key.toLowerCase();
+    window.addEventListener('keydown', function (e) {
+        const key = e.key.toLowerCase();
 
-      if (key === konamiSequence[konamiPosition].toLowerCase()) {
-        konamiPosition++;
-        if (konamiPosition === konamiSequence.length) {
-          
-          window.location.href = 'ඞ'; 
+
+        if (key === konamiSequence[konamiPosition].toLowerCase()) {
+            konamiPosition++;
+            if (konamiPosition === konamiSequence.length) {
+                alert("Konami Code activated! Redirecting...");
+                window.location.href = 'ඞ';
+            }
+        } else {
+            konamiPosition = 0;
         }
-      } else {
-        konamiPosition = 0; 
-      }
-    });
-  })();
+
+
+        if (key === secretSequence[secretPosition]) {
+            secretPosition++;
+            if (secretPosition === secretSequence.length) {
+                // Request fullscreen before redirect
+                const elem = document.documentElement;
+
+                function goFullscreenAndRedirect() {
+                    if (elem.requestFullscreen) {
+                        elem.requestFullscreen().then(() => {
+                            window.location.href = 'las.html';
+                        }).catch(() => {
+                            // If fullscreen fails, redirect anyway
+                            window.location.href = 'las.html';
+                        });
+                    } else if (elem.webkitRequestFullscreen) {
+                        elem.webkitRequestFullscreen();
+                        window.location.href = 'las.html';
+                    } else if (elem.msRequestFullscreen) {
+                        elem.msRequestFullscreen();
+                        window.location.href = 'las.html';
+                    } else {
+                        // No fullscreen API, just redirect
+                        window.location.href = 'las.html';
+                    }
+                }
+
+                goFullscreenAndRedirect();
+            }
+        } else {
+            secretPosition = 0;
+}});
+})();
